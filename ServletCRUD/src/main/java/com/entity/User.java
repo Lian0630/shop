6570baed82bbaccd84b3b9d5b82720704cb262e3
Lian0630 <67944790+Lian0630@users.noreply.github.com>
2021-06-04 17:26:27 +0800

    Add files via upload

---
+++ ServletCRUD/src/main/java/com/entity/User.java
@@ -0,0 +1,72 @@
+package com.entity;
+
+public class User {
+		
+	protected String id;
+	protected String name;
+	protected int phone;
+	protected String address;
+	protected String description;
+	@Override
+	public String toString() {
+		return "User [id=" + id + ", name=" + name + ", phone=" + phone + ", address=" + address + ", description="
+				+ description + "]";
+	}
+	
+
+	public User() {
+		
+	}
+	
+	public User(String name, int phone,String address, String description) {
+		super();
+		this.name = name;
+		this.phone = phone;
+		this.address = address;
+		this.description = description;
+	}
+
+	public User(String id, String name, int phone,String address, String description) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.phone = phone;
+		this.address = address;
+		this.description = description;		
+	}
+	
+
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getPhone() {
+		return phone;
+	}
+	public void setPhone(int phone) {
+		this.phone = phone;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	
+	
+	
+}
